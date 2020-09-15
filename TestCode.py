34921930198bfe6b077bad02d255fc5379968f9a
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2020-09-15 21:37:52 +1000

    meththods practice

--- TestCode.py
+++ TestCode.py
@@ -1,3 +1,4 @@
+"""
 def myfunc(incoming):
     l1=list(incoming)
     newStr=''
@@ -14,3 +15,37 @@ def myfunc(incoming):
 
 k=myfunc('skyline')
 print(k)
+
+"""
+import math
+import  collections
+
+"""
+def do_sqr(x):
+    return x**2
+
+myList = [1,2,3,4,5,6,7,8,9]
+
+#print(list(map(do_sqr, myList)))
+
+print(list(map(lambda j: j**2, myList)))
+
+print(list(filter(lambda x: x%2 == 0,myList)))
+"""
+
+s = 'Hello Mr. Rogers, how are you this fine Tuesday?'
+
+lowCt=0
+upCt=0
+
+ct = collections.Counter(list(s))
+print(ct)
+
+for j in ct:
+    if 97 <= ord(j) <= 122:
+        lowCt += ct[j]
+    elif 65 <= ord(j) <= 90:
+        upCt += ct[j]
+
+print(f'Uppercase count = {upCt}')
+print(f'Lowercase count = {lowCt}')
